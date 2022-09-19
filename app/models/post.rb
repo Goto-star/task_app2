54656d09be2a31e277f71ee6a7a37e2afb4c2d2c
EC2 Default User <ec2-user@ip-172-31-4-27.us-west-2.compute.internal>
2022-09-19 16:32:58 +0000

    add validate

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,10 @@ class Post < ApplicationRecord
     validates :title, presence: true
     validates :start_day, presence: true
     validates :posted_day, presence: true
+    validate :start_end_check
+    
+    def start_end_check
+        errors.add(:posted_day, "は開始日より前の日付は登録できません") unless 
+        self.start_day < self.posted_day    
+    end
 end
